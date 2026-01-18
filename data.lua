23049ad3245f0dbf45dabd8172c2061395137d51
Talandar <talandar66@protonmail.com>
2026-01-18 18:39:33 +0100

    first commit

---
+++ data.lua
@@ -0,0 +1,13 @@
+-- machines
+require("prototypes/diesel-assembling-machine")
+require("prototypes/diesel-drill")
+require("prototypes/diesel-pump-connector")
+require("prototypes/diesel-pump")
+-- inserters
+require("prototypes/diesel-fast-inserter")
+require("prototypes/diesel-long-handed-inserter")
+require("prototypes/diesel-extra-long-handed-inserter")
+require("prototypes/diesel-crane-bulk-inserter")
+-- other
+require("prototypes/recipes")
+require("prototypes/technologies")
