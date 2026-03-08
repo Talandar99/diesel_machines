c48e0535b7e6f6f41ed74d75d10f6071184180cb
Talandar <talandar66@protonmail.com>
2026-03-08 23:13:54 +0100

    diesel-offshore-pump

--- data-updates.lua
+++ data-updates.lua
@@ -1,3 +1,17 @@
 local base_categories = data.raw["assembling-machine"]["assembling-machine-3"].crafting_categories
 local diesel_categories = table.deepcopy(base_categories)
+
 data.raw["assembling-machine"]["diesel-assembling-machine"].crafting_categories = diesel_categories
+
+if mods["lignumis"] then
+	if data.raw["inserter"]["steam-inserter"] then
+		data.raw["inserter"]["steam-inserter"].next_upgrade = "fast-diesel-inserter"
+	end
+	if data.raw["inserter"]["long-handed-steam-inserter"] then
+		data.raw["inserter"]["long-handed-steam-inserter"].next_upgrade = "long-handed-diesel-inserter"
+	end
+end
+
+if mods["offshore_pump_require_power"] then
+	require("prototypes.diesel-offshore-pump")
+end
