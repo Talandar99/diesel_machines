df64440cee65c39ca19472a9e13a64a89fb4781c
Talandar <talandar66@protonmail.com>
2026-06-08 00:15:11 +0200

    cargo crates changes

--- data-updates.lua
+++ data-updates.lua
@@ -1,8 +1,3 @@
-local base_categories = data.raw["assembling-machine"]["assembling-machine-3"].crafting_categories
-local diesel_categories = table.deepcopy(base_categories)
-
-data.raw["assembling-machine"]["diesel-assembling-machine"].crafting_categories = diesel_categories
-
 if mods["lignumis"] then
 	if data.raw["inserter"]["steam-inserter"] then
 		data.raw["inserter"]["steam-inserter"].next_upgrade = "fast-diesel-inserter"
@@ -18,3 +13,17 @@ end
 if mods["pelagos"] then
 	data.raw["item"]["portable-diesel-generator"].default_import_location = "pelagos"
 end
+
+-- diesel assembling-machine recipes
+local base_categories = data.raw["assembling-machine"]["assembling-machine-3"].crafting_categories
+local diesel_categories = table.deepcopy(base_categories)
+if mods["barreling_machines"] then
+	table.insert(diesel_categories, "barreling")
+end
+if mods["cargo_crates"] then
+	table.insert(diesel_categories, "cargo-crates")
+end
+if mods["pelagos"] then
+	table.insert(diesel_categories, "diesel-rocket-parts")
+end
+data.raw["assembling-machine"]["diesel-assembling-machine"].crafting_categories = diesel_categories
---
+++ settings.lua
@@ -0,0 +1,13 @@
+local function force_setting(setting_type, setting_name, value)
+	local setting = data.raw[setting_type .. "-setting"][setting_name]
+	if setting then
+		if setting_type == "bool" then
+			setting.forced_value = value
+		else
+			setting.allowed_values = { value }
+		end
+		setting.default_value = value
+		setting.hidden = true
+	end
+end
+force_setting("bool", "cargo-crates-can-use-regular-assembling-machines", false)
